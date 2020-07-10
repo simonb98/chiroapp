d8be13d41842946c73f482489b0147024e4112cf
Simon Baeyens <43532756+simonb98@users.noreply.github.com>
2020-07-10 10:30:51 +0200

    starter files redone

--- chiro-app/src/app/app.component.spec.ts
+++ chiro-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chiroapp'`, () => {
+  it(`should have as title 'chiro-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chiroapp');
+    expect(app.title).toEqual('chiro-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('chiroapp app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('chiro-app app is running!');
   });
 });
